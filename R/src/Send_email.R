587a07b817ec4ac99acf59f2b2696d3bdc7b46d1
fjasteen <frederique.steen@inbo.be>
2024-06-28 00:00:43 +0200

    Update Send_email.R

--- R/src/Send_email.R
+++ R/src/Send_email.R
@@ -5,21 +5,23 @@ library(blastula)
 library(keyring)
 library(rmarkdown)
 
+key_set("gmail", "frederique.steen@inbo.be")
+
 # Render het RMarkdown-bestand naar HTML
 render("./src/email_report.Rmd", output_file = "../data/output/email_report.html")
 
-html_content <- paste(readLines("./email_report.html"), collapse = "\n")
+html_content <- paste(readLines("./data/output/email_report.html"), collapse = "\n")
 # Maak een e-mail met blastula
 email <- compose_email(
   body = md(paste0('<div style="max-width: 600px; margin: auto; text-align: justify;">', html_content, '</div>'))
 )
 
-# create_smtp_creds_key(
-#   id = "gmail",
-#   user = "frederique.steen@inbo.be",  # je Gmail-adres
-#   provider = "gmail",
-#   use_ssl = TRUE
-# )
+create_smtp_creds_key(
+  id = "gmail",
+  user = "frederique.steen@inbo.be",  # je Gmail-adres
+  provider = "gmail",
+  use_ssl = TRUE
+)
 
 smtp_send(
   email,
