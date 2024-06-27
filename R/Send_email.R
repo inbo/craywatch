46f8e1f218ac1ec0d4a5ba2c4f4f6fe08070bf43
fjasteen <frederique.steen@inbo.be>
2024-06-27 23:25:01 +0200

    add script to send email

---
+++ R/Send_email.R
@@ -0,0 +1,35 @@
+#install.packages("blastula")
+#install.packages("keyring")
+
+library(blastula)
+library(keyring)
+
+#key_set("gmail", "frederique.steen@inbo.be")
+rmarkdown::render("./email_report.Rmd")
+# Render het RMarkdown-bestand naar HTML
+render("email_report.Rmd", output_file = "email_report.html")
+
+html_content <- paste(readLines("./email_report.html"), collapse = "\n")
+# Maak een e-mail met blastula
+email <- compose_email(
+  body = md(paste0('<div style="max-width: 600px; margin: auto; text-align: justify;">', html_content, '</div>'))
+)
+
+# create_smtp_creds_key(
+#   id = "gmail",
+#   user = "frederique.steen@inbo.be",  # je Gmail-adres
+#   provider = "gmail",
+#   use_ssl = TRUE
+# )
+
+smtp_send(
+  email,
+  from = "frederique.steen@inbo.be",
+  to = "frederique.steen@inbo.be",
+  subject = "Craywatch Nieuws",
+  credentials = creds_key(
+    id = "gmail"
+  ),
+  verbose = TRUE,
+  timeout = 60  # Time-out in seconden
+)
