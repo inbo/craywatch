b7f7e6e62c067ae65a60ba336413f3b3b0e03530
fjasteen <frederique.steen@inbo.be>
2024-09-02 13:27:46 +0200

    New scripts first analyses

---
+++ R/src/First_Analyses.R
@@ -0,0 +1,90 @@
+library(dplyr)
+
+
+### LOAD DATA ##################################################################
+cleandata <- read.csv("./data/observations/output/cleandata.csv")
+datacheck <- read.csv("./data/observations/output/datacheck.csv")
+
+alldata <- rbind(cleandata,datacheck)
+
+localities <- read.csv("../assets/localities.csv")
+
+
+### CONSTRUCT NEW DATAFRAME ####################################################
+# Select the specified columns from the localities dataframe for sampled locID
+localities_data <- localities %>%
+  filter(locID %in% alldata$locID) %>%
+  select(NAAM, OMSCHR, vrijwillID, locID, SystemType, provincie, gemeente, CATC, Latitude, Longitude)
+
+# Add column names according to found species
+column_names <- unique(alldata$soort)
+
+for (col_name in column_names) {
+  localities_data[[col_name]] <- NA  # Initialize the new column with NA
+}
+
+
+### VOEG AANTALLEN PER SOORT TOE ###############################################
+
+#Voeg de aantallen gevangen kreeften toe per soort
+aggregated_data <- alldata %>%
+  group_by(locID, soort) %>%
+  summarise(total_individuals = sum(number.of.individuals)) %>%
+  ungroup()
+
+# Loop door elke soort om de totalen in de juiste kolommen van localities_data te plaatsen
+for (species in unique(aggregated_data$soort)) {
+  # Filter de geaggregeerde data voor de huidige soort
+  species_data <- aggregated_data %>%
+    filter(soort == species) %>%
+    select(locID, total_individuals)
+  
+  # Voeg de data toe aan de juiste kolom in localities_data
+  localities_data <- localities_data %>%
+    left_join(species_data, by = "locID") %>%
+    mutate(!!species := coalesce(total_individuals, get(species))) %>%
+    select(-total_individuals) # Verwijder tijdelijke kolom
+}
+
+
+### VOEG HET TOTAAL AANTAL VANGSTDAGEN VOOR DE LOCID TOE #######################
+
+vangstdagen_data <- alldata %>%
+  group_by(locID) %>%
+  summarise(vangstdagen = n_distinct(date)) %>%
+  ungroup()
+
+# Samenvoegen met localities_data
+localities_data <- localities_data %>%
+  left_join(vangstdagen_data, by = "locID")
+
+print(localities_data)
+
+### ZIJN DE DAGEN OPEENVOLGEND? ################################################
+
+# Stap 1: Controle op opeenvolgende dagen
+consecutive_data <- alldata %>%
+  arrange(locID, date) %>%  # Sorteren op locID en datum
+  group_by(locID) %>%
+  summarise(consecutive = all(diff(as.Date(date)) == 1)) %>%
+  ungroup()
+
+# Stap 2: Samenvoegen met localities_data
+localities_data <- localities_data %>%
+  left_join(consecutive_data, by = "locID")
+
+
+### ZIJN DE DATA CLEAN ? 4 VANGSTDAGEN + OPEENVOLGEND ##########################
+# Stap 1: Toevoegen van de clean-status kolom
+localities_data <- localities_data %>%
+  mutate(clean_data = locID %in% cleandata$locID)
+
+# Bekijk de bijgewerkte tabel
+print(localities_data)
+
+
+# Find locID values that are in combined_locID but not in localities$locI
+erratic_locID <- setdiff(alldata$locID, localities$locID)
+
+write.csv(localities_data, "./data/observations/output/first_data.csv", row.names = FALSE)
+
---
+++ R/src/Update_observations.R
@@ -0,0 +1,110 @@
+library(dplyr)
+
+#####GET NEW DATA FROM NATUURPUNT EXPORT########################################
+#By comparing the id from the observations in the last_import & the new import only new observations are considered
+# Set the directory path
+directory_path <- "./data/observations/"
+
+# Get the earlier import or initialize an empty data frame
+if (file.exists("./data/observations/output/last_import.csv")) {
+  last_import <- read.csv("./data/observations/output/last_import.csv")
+} else {
+  last_import <- data.frame(id = character(), stringsAsFactors = FALSE)
+}
+
+# Get the latest export files from Natuurpunt (manually downloaded from eventpage)
+# List files in the directory that match the pattern
+files <- list.files(directory_path, pattern = "^event-craywatch-.*\\.csv$", full.names = TRUE)
+
+# Function to read and process each file, i.e. add species as column
+process_file <- function(file) {
+  # Extract the species name from the file name
+  species <- gsub("event-craywatch-(.*)\\.csv", "\\1", basename(file))
+  species <- gsub("-", " ", species)
+  
+  # Read the file into a data frame
+  data <- read.csv(file)
+  
+  # Only add the species column if the data is not empty
+  if (nrow(data) > 0) {
+    data$soort <- species
+  }
+  
+  return(data)
+}
+
+# Process all files and combine them into one data frame
+all_data <- lapply(files, function(file) {
+  df <- process_file(file)
+  # Only return non-empty data frames
+  if (nrow(df) > 0) return(df)
+}) %>% bind_rows()
+
+
+# sorted by locID
+all_data <- arrange(all_data, Code.sample.location)
+
+# Only keep the new data
+new_data <- all_data %>% 
+  filter(!(id %in% last_import$id))
+
+
+###CORRECT UGLY locID names ####################################################
+# Get all data that has incorrect format in Code.sample.location
+# Define the pattern
+pattern <- ".*?([IV]_[0-9]{4}_[0-9]{1,2}).*"
+
+# Replace all '-' and '.' with '_'
+new_data$locID <- gsub("[-. ]", "_", new_data$Code.sample.location)
+# Replace 'v' with 'V' and 'i' with 'I' in the locID column
+new_data$locID <- gsub("v", "V", new_data$locID)
+new_data$locID <- gsub("i", "I", new_data$locID)
+
+new_data$locID <- gsub(pattern, "\\1", new_data$locID)
+
+
+###FILTER DATA FOR 4 CONSECUTIVE CATCH DAYS ####################################
+#Data met opeenvolgende vangstdagen
+# Append the new data to the datacheck of previous download to filter the 4 consecutive catch days
+if (file.exists("./data/observations/output/datacheck.csv")) {
+  old_datacheck <- read.csv("./data/observations/output/datacheck.csv")
+} else {
+  old_datacheck <- data.frame(locID = character(), date = character(), stringsAsFactors = FALSE) 
+}
+datacheck <- rbind(old_datacheck, new_data)
+
+
+
+# Bereken aantal entries per Code.sample.location en controleer opeenvolgende dagen
+locID_group <- datacheck %>%
+  group_by(locID) %>%
+  summarise(
+    n = n(),
+    consecutive_days = all(diff(as.Date(date, format = "%Y-%m-%d")) == 1, na.rm = TRUE)
+  )
+
+# Filter voor entries met n == 4 en consecutive_days == TRUE
+data_approved <- locID_group %>%
+  filter(n == 4, consecutive_days == TRUE)
+
+# Behoud enkel data met valide locID om naar clean data te schrijven
+data_approved <- data_approved[grep(pattern, data_approved$locID), ]
+
+# Voeg de nieuwe cleandata toe aan de eerdere
+cleandata <- datacheck %>% filter(locID %in% data_approved$locID)
+
+# Voeg de nieuwe cleandata toe aan de eerdere als die bestaat
+if (file.exists("./data/observations/output/cleandata.csv")) {
+  old_cleandata <- read.csv("./data/observations/output/cleandata.csv")
+} else {
+  old_cleandata <- data.frame(locID = character(), date = character(), stringsAsFactors = FALSE)
+}
+
+cleandata <-rbind(old_cleandata, cleandata)
+
+datacheck <- datacheck %>% filter(!(locID %in% data_approved$locID))
+
+# Save datafiles voor volgende analyse
+write.csv(datacheck, "./data/observations/output/datacheck.csv", row.names = FALSE)
+write.csv(cleandata, "./data/observations/output/cleandata.csv", row.names = FALSE)
+write.csv(all_data, "./data/observations/output/last_import.csv", row.names = FALSE)
