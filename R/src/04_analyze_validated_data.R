d158926eb6c8bd9d410d963a466ddddc8609dc95
margotvermeylen <168713182+margotvermeylen@users.noreply.github.com>
2025-09-18 14:48:05 +0200

    finaliseer craywatch dataset

--- R/src/04_analyze_validated_data.R
+++ R/src/04_analyze_validated_data.R
@@ -1,7 +1,7 @@
 # ====================================================
 # Scriptnaam: 04_analyze_validated_data.R
 # Auteur: Margot Vermeylen
-# Datum: 09-01-2025 (update 28-08-2025)
+# Datum: 09-01-2025 (update 18-09-2025)
 # Beschrijving: 
 # Dit script haalt de bruikbare Craywatch data uit de gevalideerde datas met waarnemingen
 # en creërt de finale dataset
@@ -35,7 +35,7 @@ craywatch_data <- craywatch_data %>%
   ) %>%
   ungroup()
 
-# 2: som van vallen en individuen per dag en per locatie per soort en per sessie
+# 2: som van vallen en individuen per dag en per locatie, soort en sessie
 daily_data <- craywatch_data %>%
   group_by(locID, session_id, date, soort) %>%
   summarize(
@@ -47,7 +47,7 @@ daily_data <- craywatch_data %>%
          
 # 3: som van de dagelijkse data per locatie en per soort
 grouped_craywatch_data <- daily_data %>%
-  group_by(locID, session_id, soort) %>%  # Groepeer per locatie, sessie EN soort
+  group_by(locID, session_id, soort) %>%  # Groepeer per locatie, sessie en soort
   summarize(
     individuals.caught = sum(individuals_daily, na.rm = TRUE), 
     days_sampled = n_distinct(date), 
@@ -62,36 +62,36 @@ grouped_craywatch_data <- daily_data %>%
     .groups = 'drop',
     vrijwillID = first(vrijwillID),
   ) %>%
+  mutate(
+    by_protocol = if_else(
+      (soort == "crayfish indet" & traps_used >= 12) | (soort != "crayfish indet"),
+      TRUE,
+      FALSE
+    )
+  ) %>%
   select(-session_id) # Verberg de session_id kolom
 
 # Selecteer de kolommen 'locID', 'Latitude', and 'Longitude' van localities
 localities_selected <- map_data %>%
-  dplyr::select(locID, Latitude, Longitude)
+  dplyr::select(locID, Latitude, Longitude, CATC, VHAG)
 
 # voeg data samen met localities om Latitude en Longitude toe te voegen obv locID
 grouped_craywatch_data <- left_join(grouped_craywatch_data, localities_selected, by = "locID")
 
-########## vanaf hier aanpassen ###########
-# Nagaan hoeveel mensen exact het protocol volgden
-protocol_followers <- grouped_craywatch_data %>%
-  filter(days_sampled == 4 & consecutive == "TRUE") # locaties waar er exact 4 na elkaar dagen gecontroleerd is
-
-n_distinct(protocol_followers$vrijwillID)
-
-# Filter the data die we willen gebruiken
-craywatch_data_usable <- grouped_craywatch_data %>%
-  filter(!is.na(Longitude) & !is.na(Latitude)) %>%
+# Ontbrekende locIDs en coördinaten er uit halen
+grouped_craywatch_data <- grouped_craywatch_data %>%
   mutate(
     Longitude = as.numeric(Longitude),
     Latitude = as.numeric(Latitude)) %>%
-  filter(!(species == "absent" & days_sampled < 4))  # Exclude rows where species is "absent" and number_of_days < 4
+  filter(!is.na(Longitude) & !is.na(Latitude))
 
 # Sla finale Craywatch data op als een CSV-bestand
-write.csv(craywatch_data_usable, "~/GitHub/craywatch/R/data/output/final_craywatch_data_all.csv", row.names = FALSE)
+write.csv(grouped_craywatch_data, "~/GitHub/craywatch/R/data/output/final_craywatch_data_all.csv", row.names = FALSE)
+
+craywatch_data_usable <- grouped_craywatch_data %>%
+  filter(by_protocol == TRUE)
 
 # Maak GIS-laag
 craywatch_sf <- st_as_sf(craywatch_data_usable, coords = c("Longitude", "Latitude"), crs = 4326)
 st_write(craywatch_sf, "~/GitHub/craywatch/R/data/output/GIS-laag/craywatch_results.shp", append=FALSE)
 st_write(craywatch_sf, "~/GitHub/craywatch/R/data/output/GIS-laag/craywatch_results.gpkg", layer = "craywatch_observations", append=FALSE)
-
-
